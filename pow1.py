c49894c42b244dac061b283c70f21bd609c31f98
tiwaria00172 <tiwariabhishek00172@gmail.com>
2026-04-22 19:41:56 +0530

    Create pow1.py

---
+++ pow1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x, n):
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        result = 1
+        
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
+            
+            x *= x
+            n //= 2
+        
+        return result
